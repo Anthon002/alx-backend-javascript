a1a00899da81eceaa0fbd5598c0d111e7454194e
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-04 18:48:26 +0100

    Advanced task 1

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
 export default function createIteratorObject(report) {
   return (function* _() {
-    for (const dept of Object.values(report.allEmployees)) {
-      for (const emp of dept) {
-        yield emp;
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
     }
   }());
