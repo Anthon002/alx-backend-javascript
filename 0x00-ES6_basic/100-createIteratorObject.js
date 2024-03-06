ed2dfb9d3475b68558126475c11b95646c38de71
Anthon002 <chineduanulugwo@gmail.com>
2024-03-05 22:38:56 -0800

    update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,1 @@
 export default function createIteratorObject(report) {
-  return (function* _() {
-    for (const dept of Object.values(report.allEmployees)) {
-      for (const emp of dept) {
-        yield emp;
-      }
-    }
-  }());
-}
