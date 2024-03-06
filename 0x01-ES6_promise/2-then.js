ad53a53352526402e13ab0a8a73500deb014d149
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-06 12:19:12 +0100

    using then with promise

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function handleResponseFromAPI(promise){
+    return new Promise((resolve, reject)=> {
+        if (promise){
+            resolve({
+                status: 200,
+                body: "success",
+            })
+        }else{
+            new Error();
+        }
+    })
+    .then(console.log("Got a response from the API"))
+}
