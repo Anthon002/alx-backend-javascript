32bf96fc8b205cf6d59afaa1d55b98d5061b3f79
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-08 21:48:52 +0100

    getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,38 +1,16 @@
-class HolbertonCourse{
-    constructor(name, length, students){
-        this.name = name;
-        this.length = length;
-        this.students = students;
-    }
-    
-    get name(){
-        return this._name;
-    }
-    
-    set name(value){
-        if(typeof(value) !== 'string')
-        {
-            throw new TypeError('Name must be a string')
-        }
-        this._name = value
-    }
-    
-    get length(){
-        return this._length;
-    }
-    
-    set length(value){
-        if(typeof(value) !== 'number')
-        {
-            throw new TypeError('Length must be a number')
-        }
-        this._length = value;
-    }
-    get students(){
-        return this._students
-    }
-    
-    set students(value){
+class HolbertonCourse {
+
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+  
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
     if (!(value instanceof Array)) {
       throw new TypeError('Students must be an array of strings');
     }
@@ -40,5 +18,27 @@ class HolbertonCourse{
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
+  }
+  
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
 }
