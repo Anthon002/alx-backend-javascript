e6ad9fda8e7b968670b88399a5a47977514c5433
Anthon002 <chineduanulugwo@gmail.com>
2024-03-12 10:51:09 -0700

    filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,10 +1,12 @@
 export default function getStudentsByLocation(data, location){
     let objarr = []
+	if (Array.isArray(data)){
     for (let i of data){
         if (i.location == location){
             objarr.push(i);
         }
     }
+	}
     return objarr    
     
 }
