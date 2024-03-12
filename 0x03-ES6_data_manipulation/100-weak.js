9fb295b048a120719b5e87518b05ad26c43cdb74
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-03-12 19:37:18 +0100

    weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,12 @@
 export const weakMap = new WeakMap();
+const max_calls = 5;
 
-const MAXENDPOINTCALLS = 5;
-
-function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAXENDPOINTCALLS) {
+  if (weakMap.get(endpoint) >= max_calls) {
     throw new Error('Endpoint load is high');
   }
 }
-export queryAPI;
