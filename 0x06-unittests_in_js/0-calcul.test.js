fbfe6236eecf7e0a618caee93d4ef92729369544
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-05-02 22:29:59 +0100

    testing calculate number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,20 +6,20 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  it('rounding down b\'s floating point fractional number', () => {
-    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
-  });
-
   it('rounding down a and b\'s floating point fractional number', () => {
     assert.strictEqual(calculateNumber(1.4, 2.4), 3);
   });
 
-  it('rounding down a\'s floating point fractional number', () => {
-    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
   it('rounding up b\'s floating point fractional numbers', () => {
     assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
   });
 
   it('rounding up a and b\'s floating point fractional numbers', () => {
