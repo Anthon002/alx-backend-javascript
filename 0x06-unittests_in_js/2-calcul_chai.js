8f07dd0bb2d3023afdf31b2906fe1e2b1dbdc7a3
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-05-03 06:01:44 +0100

    math_round

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,9 +1,11 @@
 const calculateNumber = (type, a, b) => {
   if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
+    math_round = Math.round(a) + Math.round(b);
+    return math_round
   }
   if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
+    math_round_sub = Math.round(a) - Math.round(b);
+    return math_round_sub
   }
   if (type === 'DIVIDE') {
     return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
