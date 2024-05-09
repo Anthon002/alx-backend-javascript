06012a2904c21776a126db237a63ed30182e4afc
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-05-09 06:17:15 +0100

    Deferring to a new cohort

--- 0x06-unittests_in_js/3-payment.js
+++
@@ -1,8 +0,0 @@
-const Utils = require('./utils');
-
-const sendPaymentRequestToApi = (totalAmount, totalShipping){
-  const total_cost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total_cost}`);
-};
-
-module.exports = sendPaymentRequestToApi;
