e90a24d6da2ed16ffe3793c02fa35f4a9d3885c1
Anthon002 <chineduanulugwo@gmail.com>
2024-07-22 20:13:34 +0100

    changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,13 @@
-/**
- * this nodejs script to be executed through the command line
- */
-const readline = require("readline");
-const rl_interface = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-console.log("Welcome to Holberton School, what is your name?")
-
-rl_interface.on('line',(input)=> {
-	console.log(`Your name is: ${input}`)
-	rl_interface.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-rl_interface.on('close',()=>{
-})
-
-rl_interface.on('end',()=>{
-	console.log("This important software is now closing\n")
-})
