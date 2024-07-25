967d656bb28ef9121afb1a63fe20cc61cddf6845
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-07-25 14:06:41 +0100

    task 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,29 +1,29 @@
-const sinon = require('sinon');
-const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./5-payment');
+var sinon = require('sinon');
+var { expect } = require('chai');
+var sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let bigBrother;
+  let consoleSpy;
 
   beforeEach(() => {
-    if (!bigBrother) {
-      bigBrother = sinon.spy(console);
+    if (!consoleSpy) {
+      consoleSpy = sinon.spy(console, 'log');
     }
   });
 
   afterEach(() => {
-    bigBrother.log.resetHistory();
+    consoleSpy.resetHistory();
   });
 
-  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+  it('should log the total for inputs 100 and 20', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
   });
 
-  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+  it('should log the total for inputs 10 and 10', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
   });
 });
