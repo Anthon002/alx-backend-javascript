a0e149eeac0a66c424204cd04bd28e11f2e024af
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-07-25 14:04:15 +0100

    task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,20 @@
-const sinon = require('sinon');
-const Utils = require('./utils');
-const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./4-payment');
+var Utils = require('./utils');
+var sinon = require('sinon');
+var sendPaymentRequestToApi = require('./4-payment');
+var { expect } = require('chai');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const bigBrother = sinon.spy(console);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
+  it('sendPaymentRequestToApi will call console.log using the right arguments', () => {
+    var big_brother = sinon.spy(console);
+    var dummy = sinon.stub(Utils, 'calculateNumber');
 
     dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
     expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
+    expect(big_brother.log.calledWith('The total is: 10')).to.be.true;
+    expect(big_brother.log.callCount).to.be.equal(1);
     dummy.restore();
-    bigBrother.log.restore();
+    big_brother.log.restore();
   });
 });
