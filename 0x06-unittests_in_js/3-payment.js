5a40593cf9c65912aba65dac9cdd4f902e52b977
Chinedu Anulugwo <Chineduanulugwo@gmail.com>
2024-07-25 13:59:30 +0100

    task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require('./utils');
 
-const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
+function sendPaymentRequestToApi(totalAmount, totalShipping){
+  var total_cost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total_cost}`);
 };
 
 module.exports = sendPaymentRequestToApi;
